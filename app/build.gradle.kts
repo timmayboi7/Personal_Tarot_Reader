46f927c1dd0cca3180cf794a4404d913bbe7122f
timmayboi7 <timmayboi7@gmail.com>
2025-11-17 22:17:47 -0600

    Apply Compose compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.dagger.hilt.android")
     id("androidx.room")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("androidx.room") version "2.6.1" apply false
