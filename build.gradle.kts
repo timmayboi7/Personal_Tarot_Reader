961b663c97fe23ba1f23bfd9c909f6a52fe0ebca
timmayboi7 <timmayboi7@gmail.com>
2025-11-17 23:32:44 -0600

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-val kotlinVersion = "2.0.20"
 
 plugins {
     id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
-    id("org.jetbrains.kotlin.plugin.compose") version kotlinVersion apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("androidx.room") version "2.6.1" apply false
     id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
